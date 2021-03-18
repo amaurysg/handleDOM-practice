752c59fd5b2d2f6d6842473f74097bd982ecb31c
amaurysg <amaurysg@users.noreply.github.com>
2021-03-18 20:16:25 +0000

    deploy: f276f10f7dd1a7dc67cbfc9d8420044d78736421

--- _dist_/index.js
+++ _dist_/index.js
@@ -7,26 +7,58 @@ console.log('Happy hacking 😄')
 
 const url = `https://platzi-avo.vercel.app/api/avo`
 
+
+//API Intl, api de internacionalización de moneda / fecha
+const formatPrice = (price)=>{
+
+  const newPrice = new window.Intl.NumberFormat('en-EN',
+  { style: 'currency', 
+  currency:'USD'}).format(price)
+  return newPrice
+
+}
+
 const getAvo = async ()=>{
   const response = await fetch(url)
   const {data} = await response.json()
-
+//create items as array empty
   const items = []
-    data.forEach( n =>{
-        console.log(n.name)
-        //Creamos elementos
-
+  //Create container in main body in this case id='app'
+  const appNode = document.querySelector('#app')
+ 
+    data.forEach( avo =>{
+        console.log(avo.attributes)
+        //Create Elements
         const img = document.createElement('img')
+        img.src= `https://platzi-avo.vercel.app/${avo.image}` 
+        img.className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
+        
+        
+        const description = document.createElement('p')
+        description.textContent= avo.attributes.description
+        description.className = 'text-lg font-semibold '
+
         const title = document.createElement('h2')
+        title.textContent=avo.name
+        title.className = 'text-lg font-semibold '
+        
+
         const price = document.createElement('div')
-
-         const container = document.createElement('div')
-         container.append(img, title, price)
-
-         items.push(container)
+        price.className="text-gray-600"
+        price.textContent = formatPrice(avo.price)
+        //Create container
+        const container = document.createElement('div')
+        container.className="md:flex bg-gray-100 rounded-xl p-8 md:p-0"
+        // container.className="flex flex-wrap"
+        //Add elements to container
+        container.append(img, description, title, price)
+        //Add container to conts items
+        items.push(container)
 
         })
-        document.body.append(...items)
+        //Add to DOM items, we can use spread operator
+      
+        appNode.append(...items)
 
   return data
 }
